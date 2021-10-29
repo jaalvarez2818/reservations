7f0a19c6ca849463e0c6caa25e2ede3fd2bfc931
Jose Angel <jaalvarez2818@gmail.com>
2021-10-29 17:44:26 -0400

    - Add wkhtmltopdf.

--- reservations/settings.py
+++ reservations/settings.py
@@ -167,8 +167,7 @@ if platform.system() == 'Windows':
     PDF_CONFIG = pdfkit.configuration(
         wkhtmltopdf=os.environ.get('WKHTMLTOPDF_BINARY', 'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'))
 else:
-    PDF_CONFIG = pdfkit.configuration(
-        wkhtmltopdf=os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf'))
+    PDF_CONFIG = pdfkit.configuration(wkhtmltopdf='./bin/wkhtmltopdf')
     # WKHTMLTOPDF_CMD = os.subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')],
     #                                       stdout=os.subprocess.PIPE).communicate()[0].strip()
     # PDF_CONFIG = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
